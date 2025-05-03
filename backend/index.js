71858de770aa0bdad0ced94abc6ebedf92355919
AhmedSherif2002 <113216034+AhmedSherif2002@users.noreply.github.com>
2025-05-03 12:49:31 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,14 @@ const app = express();
 const port = process.env.PORT | 5000;
 console.log("v222")
 let isReady = false;
-let isAlive = true;
+let isAlive = false;
 setTimeout(()=>{
   isReady = true;
-  setTimeout(()=>{
-    isAlive = false;
-  }, 100000) // not alive after a minute since ready.
-}, 20000); // Ready After 50 seconds
-
+//   setTimeout(()=>{
+//     isAlive = false;
+//   }, 60000) // not alive after a minute since ready.
+// }, 20000); // Ready After 20 seconds
+  
 app.get("/ready", (req, res)=>{
   if(isReady)
     res.status(200).send("Ready");
