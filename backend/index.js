e94fbc1cec763ff2a78e1da4f32a45954595b3a0
AhmedSherif2002 <113216034+AhmedSherif2002@users.noreply.github.com>
2025-05-03 11:36:35 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT | 5000;
-console.log("vv222")
+console.log("v222")
 let isReady = false;
 let isAlive = true;
 setTimeout(()=>{
